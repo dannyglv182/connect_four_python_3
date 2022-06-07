90358433cc114877e0b699c8d50bc1c11db3e76c
Danny <dagalvez@pdx.edu>
2022-06-07 23:10:10 +0000

    Moved lists containing game status data to their own file.

--- gameboard.py
+++ gameboard.py
@@ -4,7 +4,7 @@
 """
 
 import random
-
+from lists import *
 # list of lists representing slots on a 4*4 gameboard
 board = [[1, 1], [1, 2], [1, 3], [1, 4],
          [2, 1], [2, 2], [2, 3], [2, 4],
